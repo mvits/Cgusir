b7d8981a6a44304ca1ddbb0be8990d006ea18ac8
stiv15 <stivmarquez.ud@gmail.com>
2017-06-05 22:03:55 -0500

    Implementacón de Plantilla y Plugins

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,21 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+// Rutas Bilblioteca
+/*
+Route::group(['prefix' => 'biblioteca'], function () {
+
+Route::get('autores/{nombre?}', function ($nombre = 'stiv') {
+
+var_dump($nombre);
+
+});
+
+});
+ */
