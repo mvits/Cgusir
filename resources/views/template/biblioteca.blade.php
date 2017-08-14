4488c83cc4d1681cfbda705818dae0397de197be
stiv15 <stivmarquez.ud@gmail.com>
2017-08-13 20:41:21 -0500

    Creación de Plantilla de Biblioteca (Blade)

--- resources/views/template/biblioteca.blade.php
+++ resources/views/template/biblioteca.blade.php
@@ -18,71 +18,20 @@
 <body>
 
     <section id='cabecera' >
-        <!-- Navbar -->
-        <div class="navbar navbar-default" role="navigation">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Biblioteca Visión Catastral</a>
-          </div>
-          <div class="navbar-collapse collapse">
-
-            <!-- Left nav -->
-            <ul class="nav navbar-nav">
-              <li><a href="#">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</span></a></li>
-                </ul>
-              </li>
-            </ul>
-
-            <!-- Right nav -->
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">A sub menu <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">Action</a></li>
-                      <li><a href="#">Another action</a></li>
-                      <li><a href="#">Something else here</a></li>
-                      <li class="disabled"><a class="disabled" href="#">Disabled item</a></li>
-                      <li><a href="#">One more link</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="#">A separated link</a></li>
-                </ul>
-              </li>
-            </ul>
-
-          </div><!--/.nav-collapse -->
-        </div>
+         @yield('menuPrincipalBiblioteca')
     </section>
     <section id='contenido' >
         <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Panel title</h3>
+            <div id='tituloFormulario' class="panel-heading">
+                <h3 class="panel-title colorGris"><b>@yield('tituloFormulario')</b></h3>
             </div>
-            <div class="panel-body">Panel content</div>
+            <div id='contenidoFormulario' class="panel-body">@yield('contenidoFormulario')</div>
         </div>
     </section>
 
     <script src="{{ asset('plugins/jquery/js/jquery-3.2.1.min.js') }}"></script>
     <script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    <!-- Script Menu Principal Biblioteca-->    
     <script src="{{ asset('plugins/smartmenus/jquery.smartmenus.min.js') }}"></script>
     <script src="{{ asset('plugins/smartmenus/addons/bootstrap/jquery.smartmenus.bootstrap.js') }}"></script>
 </body>
---
+++ resources/views/template/menuBiblioteca.blade.php
@@ -0,0 +1,45 @@
+@section('menuPrincipalBiblioteca')
+	   <!-- Navbar -->
+        <div class="navbar navbar-default" role="navigation">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Biblioteca Visión Catastral</a>
+          </div>
+          <div class="navbar-collapse collapse">
+
+            <!-- Left nav -->
+            <ul class="nav navbar-nav">
+	              <li><a href="#">Dropdown<span class="caret"></span></a>
+	                <ul class="dropdown-menu">
+	                  <li><a href="#">Action</a></li>
+	                  <li><a href="#">Another action</a></li>
+	                  <li><a href="#">Something else here</a></li>
+	                  <li class="divider"></li>
+	                  <li class="dropdown-header">Nav header</li>
+	                  <li><a href="#">Separated link</a></li>
+	                  <li><a href="#">One more separated link</span></a></li>
+	               </ul>
+	             </li> 
+            </ul>
+
+            <!-- Right nav -->
+            <ul class="nav navbar-nav navbar-right">
+					<li><a href="{{ route('principal') }}"><span class="glyphicon glyphicon-home"> CGUIR</a></li>
+						<li><a href="#"><span class="glyphicon glyphicon-user"> Usuario</span></a>
+						<ul class="dropdown-menu">
+		                  <li class="dropdown-header">Stiv Verdugo</li>
+		                  <li><a href="#">Mi Configuración</a></li>
+		                  <li class="divider"></li>
+		                  <li><a href="#">Salir</a></li>
+		                 </ul>
+	    			</li>
+            </ul>
+
+          </div><!--/.nav-collapse -->
+        </div>
+@endsection
