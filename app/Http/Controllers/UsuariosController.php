8f855d8510268032c886e5fc73cf5ecc70d5ccd3
stiv15 <stivmarquez.ud@gmail.com>
2017-08-15 02:21:11 -0500

    Desarrollo Parcial Controlador Usuarios :  Creación de Usuario Base de Datos y Estruturación del Index de Usuarios

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Usuario;
 use Illuminate\Http\Request;
 
 class UsuariosController extends Controller
@@ -9,17 +11,23 @@ class UsuariosController extends Controller
     //
 
     public function index()
-    {}
+    {
+        $usuarios = Usuario::orderBy('id', 'ASC')->simplePaginate(6);
+        return view('biblioteca.admin.usuarios.index')->with('usuarios', $usuarios);
+    }
 
     public function create()
     {
-
         return view('biblioteca.admin.usuarios.create');
-
     }
 
     public function store(Request $request)
-    {}
+    {
+        $usuario           = new Usuario($request->all());
+        $usuario->password = bcrypt($request->password);
+        $usuario->save();
+        dd('Registro Exitoso');
+    }
 
     public function show($id)
     {}
