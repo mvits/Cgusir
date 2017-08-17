f7a6103dff19bd04732291ed8fa2f6dbde2af3a4
stiv15 <stivmarquez.ud@gmail.com>
2017-08-17 01:50:48 -0500

    Implementación de metodos para actualizar Usuarios y Implementación de Validación de los Request

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UsuarioRequest;
 use App\Usuario;
-use Illuminate\Http\Request;
 use Laracasts\Flash\Flash;
 
 class UsuariosController extends Controller
@@ -22,7 +22,7 @@ class UsuariosController extends Controller
         return view('biblioteca.admin.usuarios.create');
     }
 
-    public function store(Request $request)
+    public function store(UsuarioRequest $request)
     {
         $usuario           = new Usuario($request->all());
         $usuario->password = bcrypt($request->password);
@@ -37,10 +37,26 @@ class UsuariosController extends Controller
     {}
 
     public function edit($id)
-    {}
+    {
+        $usuario = Usuario::find($id);
+        return view('biblioteca.admin.usuarios.edit')->with('usuario', $usuario);
+    }
 
-    public function update(Request $request, $id)
-    {}
+    public function update(UsuarioRequest $request, $id)
+    {
+
+        $usuario                        = Usuario::find($id);
+        $usuario->nombre                = $request->nombre;
+        $usuario->numero_identificacion = $request->numero_identificacion;
+        $usuario->codigo_estudiantil    = $request->codigo_estudiantil;
+        $usuario->tipo                  = $request->tipo;
+        $usuario->correo                = $request->correo;
+        $usuario->save();
+
+        Flash('Se actualizo de forma exitosa el usuario:<br><b>' . $usuario->numero_identificacion . ' - ' . $usuario->nombre . '</b>')->warning();
+        return redirect()->route('usuarios.index');
+
+    }
 
     public function destroy($id)
     {
