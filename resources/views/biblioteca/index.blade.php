34bd693c08f14ec0bc632d643da600f41e6a9c35
stiv15 <stivmarquez.ud@gmail.com>
2017-08-19 01:54:57 -0500

    Modificación ruta principal de biblioteca y creación de vista principal de la biblioteca

---
+++ resources/views/biblioteca/index.blade.php
@@ -0,0 +1,19 @@
+@extends('template.biblioteca')
+@extends('template.menuBiblioteca')
+
+
+@section('contenidoFormulario')
+	<h1 class="titulo">
+		<b>Biblioteca Visión Catastral
+		<br>
+		Grupo Estudiantil
+		</b>
+	</h1>
+	<br>
+	<br>
+	<p>
+		Plataforma para la gestión de recursos bibliográficos (fisicos y electrónicos) del grupo de visión catastral.<br>
+		<br>
+		La razón principal colocar a dispoción de la comunidad estudiantil recursos bibliográficos que fueron donados en el transcurrir del tiempo a travez estudiantes y docentes.
+	</p>
+@endsection
