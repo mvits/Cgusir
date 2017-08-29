fd2fa4c975d114655cfefe3d6dba2b74af83af21
stiv15 <stivmarquez.ud@gmail.com>
2017-08-28 22:35:52 -0500

    Desarrollo de Controllador (Edición y eliminar Recursos Bibliográficos)

--- app/Http/Controllers/LibrosController.php
+++ app/Http/Controllers/LibrosController.php
@@ -10,9 +10,14 @@ use Laracasts\Flash\Flash;
 
 class LibrosController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $libros = Libro::orderBy('id', 'DESC')->simplePaginate(6);
+
+        $libros = Libro::busquedaRecursos($request->titulo)->orderBy('id', 'DESC')->simplePaginate(6);
+        $libros->each(function ($libros) {
+            $libros->areas;
+            $libros->autores;
+        });
 
         return view('biblioteca.admin.libros.index')->with('libros', $libros);
     }
@@ -43,4 +48,53 @@ class LibrosController extends Controller
         return redirect()->route('libros.index');
 
     }
+
+    public function edit($id)
+    {
+
+        $libro = Libro::find($id);
+
+        $libro->each(function ($libro) {
+            $libro->areas;
+            $libro->autores;
+        });
+
+        $autores = Autor::orderBy('nombre', 'ASC')->pluck('nombre', 'id');
+        $areas   = AreaConocimiento::orderBy('nombre', 'ASC')->pluck('nombre', 'id');
+
+        return view('biblioteca.admin.libros.edit')
+            ->with('libro', $libro)
+            ->with('areas', $areas)
+            ->with('autores', $autores);
+
+    }
+
+    public function update(Request $request, $id)
+    {
+        $libro = Libro::find($id);
+        $libro->fill($request->all());
+        $libro->save();
+
+        $libro->areas()->sync($request->areas);
+        $libro->autores()->sync($request->autores);
+
+        Flash('Se actualizo de forma exitosa el recurso bibliográfico: <br><b>' . $libro->titulo . '</b>')->warning();
+
+        return redirect()->route('libros.index');
+    }
+
+    public function destroy($id)
+    {
+
+        $libro = Libro::find($id);
+        $libro->autores()->detach();
+        $libro->areas()->detach();
+        $libro->delete();
+
+        Flash('Se elimino de forma exitosa el recurso bibliográfico: <br><b>' . $libro->titulo . '</b>')->error();
+
+        return redirect()->route('libros.index');
+
+    }
+
 }
